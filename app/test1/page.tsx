a71f9e4757f0764d257beb58cd41cb8d350d41d5
alanyan2005 <2279542@bdeb.qc.ca>
2024-03-25 13:37:05 -0400

    page test

--- app/test1/page.tsx
+++ app/test1/page.tsx
@@ -1,7 +1,14 @@
 import Example from "./chart"
+import { sql } from "@vercel/postgres";
+import { Card, Title, Text } from '@tremor/react';
+import Search from "../search";
+import UsersTable from "../table";
 
 export default async function TestPage() {
-    return <p>
-
-    </p>
+    return (<main className="p-4 md:p-10 mx-auto max-w-7xl">
+        <Title>Users</Title>
+        <Text>A list of users retrieved from a Postgres database.</Text>
+        <Search />
+        <Example />
+    </main>);
 }
