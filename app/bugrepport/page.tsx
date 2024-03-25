6ca5149fa7f9e46e8e1d165bd16d1f96b222f095
alanyan2005 <2279542@bdeb.qc.ca>
2024-03-25 17:06:30 -0400

    bugrepport text

--- app/bugrepport/page.tsx
+++ app/bugrepport/page.tsx
@@ -2,7 +2,7 @@ import { Text } from "@tremor/react";
 
 export default async function TestPage() {
     return (<main className="p-4 md:p-10 mx-auto max-w-7xl">
-        <Text className="mt-4" color="blue">
+        <Text className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
             Pour tout problème envoyer un courriel à : raoufbabari@bdeb.qc.ca
         </Text>
     </main>);
