4fe5a3cbb35072fd1c12472564eb6de6493d3e01
TriFl4sh1 <triflash1.clouthier@gmail.com>
2024-03-25 13:51:33 -0400

    test

--- app/test1/chart.tsx
+++ app/test1/chart.tsx
@@ -8,7 +8,7 @@ export default function Example() {
       <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
         Sales
       </h4>
-      <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+      <p className="text-tremor-metric font-bold text-tremor-content-strong dark:text-dark-tremor-content-strong">
         $71,465
       </p>
       <p className="mt-4 flex items-center justify-between text-tremor-default text-tremor-content dark:text-dark-tremor-content">
