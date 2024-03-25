aa087d9517a22449d17c761afd317d17f18c0669
alanyan2005 <2279542@bdeb.qc.ca>
2024-03-25 13:38:42 -0400

    page test2

--- app/test1/page.tsx
+++ app/test1/page.tsx
@@ -6,9 +6,6 @@ import UsersTable from "../table";
 
 export default async function TestPage() {
     return (<main className="p-4 md:p-10 mx-auto max-w-7xl">
-        <Title>Users</Title>
-        <Text>A list of users retrieved from a Postgres database.</Text>
-        <Search />
         <Example />
     </main>);
 }
