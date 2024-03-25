0c80a86c8783d4fb9bd853999be8fa89387d4905
TriFl4sh1 <triflash1.clouthier@gmail.com>
2024-03-25 13:26:48 -0400

    new Cards

--- app/test1/chart.tsx
+++ app/test1/chart.tsx
@@ -1,1 +1,15 @@
 'use client'
+
+import { Card } from '@tremor/react';
+
+export default function Example() {
+  return (<Card className="mx-auto max-w-md">
+    <h4 className='text-tremor-default text-tremor-content dark:text-dark-tremor-content'>
+        Sales
+    </h4>
+    <p className='text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong'>
+        $71465
+    </p>
+  </Card>
+  );
+}
