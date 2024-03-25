ae76ed7ee94af59e36535fc02d62c867428a8baa
alanyan2005 <2279542@bdeb.qc.ca>
2024-03-25 16:00:10 -0400

    new line chart

--- app/test1/page.tsx
+++ app/test1/page.tsx
@@ -3,15 +3,32 @@ import { sql } from "@vercel/postgres";
 import { Card, Title, Text } from '@tremor/react';
 import Search from "../search";
 import UsersTable from "../table";
+import { LineChartHero } from "./linechart";
+
+interface Test {
+    a1: number;
+    a2: string;
+    a3: boolean;
+}
+
+const Object = [
+    
+]
 
 export default async function TestPage() {
     return (<main className="p-4 md:p-10 mx-auto max-w-7xl">
-        <div>
-            Page test
-        </div>
-        <Card>
-            Allo
+        <Title>
+            Test
+        </Title>
+        <Card className="mt-4 mb-4">
+            <Title className="pb-5">
+                Titre
+            </Title>
+            <Text className="mt-4">
+                Card
+            </Text>
         </Card>
+        <LineChartHero />
         <Chart />
     </main>);
 }
