e36a6b4b27c197fac5e91396b55607c0ed5a91d7
alan <2279542@bdeb.qc.ca>
2024-04-14 16:36:53 -0400

    login update

--- app/login.tsx
+++ app/login.tsx
@@ -24,6 +24,7 @@ export default function LoginPage() {
             router.push('/profile')
         } else {
             // Handle errors
+            console.log('login error')
         }
     }
 
