17781253a6f80f35c771b9891eb14dc256072b27
alanyan2005 <2279542@bdeb.qc.ca>
2024-04-15 17:11:55 -0400

    old login

--- app/auth.ts
+++ app/auth.ts
@@ -1,29 +1,4 @@
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-import { compare } from 'bcrypt-ts';
-import { getUser } from './db';
-import { authConfig } from './auth.config';
-
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  ...authConfig,
-  providers: [
-    Credentials({
-      async authorize({ email, password }: any) {
-        let user = await getUser(email);
-        if (user.length === 0) return null;
-        let passwordsMatch = await compare(password, user[0].password!);
-        if (passwordsMatch) return user[0] as any;
-      },
-    }),
-  ],
-});
-
-{/*import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
 export const {
@@ -39,4 +14,4 @@ export const {
   pages: {
     signIn: '/sign-in'
   }
-});*/}
+});
