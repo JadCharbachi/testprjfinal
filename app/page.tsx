deed9ff3ef59e085b210c8b5d19c1862f82215f1
alanyan2005 <2279542@bdeb.qc.ca>
2024-04-29 18:05:08 -0400

    update accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,31 @@
 import { Card, Text, Title } from "@tremor/react";
 import ApiFinhubCards from "./TheAPI/apifinhubcards";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Stack } from "react-bootstrap";
 
+//À faire : affichage différent si connecté
 
 export default async function IndexPage() {
   return (
-    <main className="p-4 md:p-10 mx-auto my-auto " style={{}}>
+    <main className="flex flex-col justify-center w-100 h-100 overflow-hidden p-4 mx-auto items-center" style={{ maxHeight:"93%" , background: "content-box radial-gradient(crimson, skyblue)" }}>
       <div>
-        <h1 className="my-4 p-4 align-text-bottom" style={{ textAlign: "center", verticalAlign: "middle", fontSize: 100 }}>
+        {/*Source : https://vercel.com/templates/next.js/nextjs-portfolio-pageview-counter */}
+        <h1 className="font-bold z-10 text-4xl text-black duration-1000 cursor-default animate-title sm:text-6xl md:text-9xl bg-clip-text">
           SIMINANCE
         </h1>
       </div>
-      <div className="p-4" style={{ textAlign: "center", columnGap: 10 }}>
-        <a className="btn btn-primary mx-4" href="login" role="button">Se connecter</a>
-        <a className="btn btn-primary mx-4" href="signup" role="button">S&apos;inscrire</a>
+      <div className="p-2" style={{ textAlign: "center" }}>
+        <Stack >
+          <a style={{ color: "lightblue" }} className="btn btn-outline-primary btn-lg mt-4 mb-2" href="login" role="button"
+          >
+            <text className="font-bold">
+              Se connecter
+            </text>
+          </a>
+          <text>
+            ou <a href="signup" style={{ color: "lightblue" }} className="font-bold link-offset-2 link-underline link-underline-opacity-0 link-underline-opacity-75-hover">S&apos;inscrire</a>
+          </text>
+        </Stack>
       </div>
       {/*<Text>
           Recherches récentes
