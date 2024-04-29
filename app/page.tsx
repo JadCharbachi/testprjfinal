03520b665245de821ab084e8f67ab4963a11605f
alanyan2005 <2279542@bdeb.qc.ca>
2024-04-29 15:23:19 -0400

    nouvel accueil

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default async function IndexPage() {
   return (
-    <main className="p-4 md:p-10 mx-auto my-auto " style={{  }}>
+    <main className="p-4 md:p-10 mx-auto my-auto " style={{}}>
       <div>
-        <Title className="my-4 p-4 text-center align-text-bottom" style={{ textAlign: "center", fontSize: 100 }}>
+        <h1 className="my-4 p-4 align-text-bottom" style={{ textAlign: "center", verticalAlign: "middle", fontSize: 100 }}>
           SIMINANCE
-        </Title>
+        </h1>
       </div>
       <div className="p-4" style={{ textAlign: "center", columnGap: 10 }}>
         <a className="btn btn-primary mx-4" href="login" role="button">Se connecter</a>
-        <a className="btn btn-primary mx-4" href="signup" role="button">S'inscrire</a>
+        <a className="btn btn-primary mx-4" href="signup" role="button">S&apos;inscrire</a>
       </div>
       {/*<Text>
           Recherches récentes
