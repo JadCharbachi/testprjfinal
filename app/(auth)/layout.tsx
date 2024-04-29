35a1268a295fc8e224d1e12d7ff7216ff6bac0c3
Jad Charbachi <157416330+JadCharbachi@users.noreply.github.com>
2024-04-29 03:50:53 -0400

    travail sur l'authentification

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,21 @@
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import '../globals.css';
+
+export const metadata = {
+    title: 'Siminance',
+    description: 'Simulation de la bourse'
+}
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children } : { children: React.ReactNode }) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+            <body className= {`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+            </html>
+        </ClerkProvider>
+    )
+}
