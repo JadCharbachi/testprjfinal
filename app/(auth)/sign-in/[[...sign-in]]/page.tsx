ce30dc983909cd335bba7a524154ba0c02f5ad93
TriFl4sh1 <triflash1.clouthier@gmail.com>
2024-05-06 17:03:21 -0400

    sign-up problem

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const SignIn = dynamic(() => import('@clerk/nextjs').then(mod => mod.SignUp), { ssr: false });
+const SignIn = dynamic(() => import('@clerk/nextjs').then(mod => mod.SignIn), { ssr: false });
 
 export default function Page() {
   return (
