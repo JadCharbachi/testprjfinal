95fb58ad25c4766753d2b6ea73ae3ecf45ebe57e
TriFl4sh1 <triflash1.clouthier@gmail.com>
2024-05-13 13:41:30 -0400

    clerkprovider2.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,26 @@
-
-
-import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+import Nav from './../app/nav';
+import { Suspense } from 'react';
 import { ClerkProvider } from '@clerk/clerk-react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from '../app/layout';
 
-
-function MyApp({ Component, pageProps }: AppProps) {
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <ClerkProvider publishableKey='pk_test_d29ydGh5LXNhaWxmaXNoLTYuY2xlcmsuYWNjb3VudHMuZGV2JA'>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      </ClerkProvider>
+    <html lang="en" className="h-full bg-gray-50">
+      <Suspense fallback={<div>Loading...</div>}>
+        <body className="h-full">
+          <div>
+            <ClerkProvider publishableKey="pk_test_d29ydGh5LXNhaWxmaXNoLTYuY2xlcmsuYWNjb3VudHMuZGV2JA">
+              <Nav />
+              {children}
+              <Analytics />
+            </ClerkProvider>
+          </div>
+        </body>
+      </Suspense>
+    </html>
   );
 }
-
-export default MyApp;
